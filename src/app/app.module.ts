66e6ff3bdcadae2cc18f3e3b0c2674180a743c7d
dengchao <645111208@qq.com>
2019-06-03 23:37:32 +0800

    messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderDetailComponent } from './component/order-detail/order-detail.component';
 import { OrdersComponent } from './page/orders/orders.component';
+import { MessagesComponent } from './conponent/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderDetailComponent,
     OrdersComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
