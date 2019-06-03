2c5e7ed7dfd18cd88b3b2b2a13973959618d4545
dengchao <645111208@qq.com>
2019-06-03 23:02:21 +0800

    生产订单详情组件

---
+++ src/app/mock-data.ts
@@ -0,0 +1,23 @@
+import { Order } from './model/Order';
+import { Product } from './model/product';
+
+export const ORDERS: Order[] = [
+  { id: 11, name: '订单1' },
+  { id: 12, name: '订单2' },
+  { id: 13, name: '订单3' },
+  { id: 14, name: '订单4' },
+  { id: 15, name: '订单5' },
+  { id: 16, name: '订单6' },
+  { id: 17, name: '订单7' },
+  { id: 18, name: '订单8' },
+  { id: 19, name: '订单9' },
+  { id: 20, name: '订单10' }
+];
+export const PRODUCTS: Product[] = [
+  { id: 11, name: '产品1' },
+  { id: 12, name: '产品2' },
+  { id: 13, name: '产品3' },
+  { id: 14, name: '产品4' },
+  { id: 19, name: '产品5' },
+  { id: 20, name: '产品6' }
+];
