c19d49c560fe330ff86c084842d83fe19494bbb3
dengchao <645111208@qq.com>
2019-06-03 23:57:11 +0800

    路由完成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OrderDetailComponent } from './component/order-detail/order-detail.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { OrdersComponent } from './page/orders/orders.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: OrderDetailComponent },
+  { path: 'orders', component: OrdersComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
